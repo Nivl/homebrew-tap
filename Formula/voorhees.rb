485eb8b50b96422022300dbf532e41597970c9b6
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-30 18:44:28 -0700

    Brew formula update for voorhees version v1.0.3

--- Formula/voorhees.rb
+++ Formula/voorhees.rb
@@ -5,24 +5,44 @@
 class Voorhees < Formula
   desc "Melvin's projects."
   homepage "https://melvin.la"
-  version "1.0.0"
-  bottle :unneeded
+  version "1.0.3"
 
-  if OS.mac?
-    url "https://github.com/Nivl/voorhees/releases/download/v1.0.0/voorhees_1.0.0_Darwin_x86_64.tar.gz"
-    sha256 "1f7306c21c8af0a93529558633c10daeee33a42aa56177404cf470db993dacb4"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/Nivl/voorhees/releases/download/v1.0.0/voorhees_1.0.0_Linux_x86_64.tar.gz"
-    sha256 "0047a35ca03ff78259bd62b4d6f6415217df26ffcf332a3c3f4582cff144346d"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/Nivl/voorhees/releases/download/v1.0.0/voorhees_1.0.0_Linux_arm64.tar.gz"
-    sha256 "38e07362bc941d345b58da8e8b1939e4ff70e181b72e81ac55681d14d89291e3"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/Nivl/voorhees/releases/download/v1.0.3/voorhees_1.0.3_Darwin_x86_64.tar.gz"
+      sha256 "0cbb19fee490f0609fb0a64b9670fcc983ad9d876738ccb9fb567d4b29365ee0"
+
+      def install
+        bin.install "voorhees"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/Nivl/voorhees/releases/download/v1.0.3/voorhees_1.0.3_Darwin_arm64.tar.gz"
+      sha256 "ed24042384d2cfa709654972c1ce6419f93f8928b523f5458029a7219836510c"
+
+      def install
+        bin.install "voorhees"
+      end
+    end
   end
 
-  def install
-    bin.install "voorhees"
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/Nivl/voorhees/releases/download/v1.0.3/voorhees_1.0.3_Linux_x86_64.tar.gz"
+      sha256 "a0cfeb34ead571439409f390e44e3aae38bc65ef1dfd8f830119164e8a2f0e8d"
+
+      def install
+        bin.install "voorhees"
+      end
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/Nivl/voorhees/releases/download/v1.0.3/voorhees_1.0.3_Linux_arm64.tar.gz"
+      sha256 "6d4450aa44286c46f79a02fe4e4bcb348bc1236e2e4e4859748a77a58a04af11"
+
+      def install
+        bin.install "voorhees"
+      end
+    end
   end
 
   test do
